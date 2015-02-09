135dd29e62defb824a76afed1a081c12abcaaa83
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2015-02-09 12:07:38 -0800

    22. Creep hits player

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,7 +12,8 @@ game.PlayerEntity = me.Entity.extend({
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
-
+		this.type = "PlayerEntity";
+		this.health = 20;
 		this.body.setVelocity(5, 20);
 		// Keeps track of which direction the character is going 
 		this.facing = "right";
@@ -82,6 +83,11 @@ game.PlayerEntity = me.Entity.extend({
 
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+	loseHealth: function(damage) {
+		this.health = this.health - damage;
+		console.log(this.health);
 	},
 
 	// doesn't let you collide with the EnemyBaseEntity
@@ -272,8 +278,28 @@ game.EnemyCreep = me.Entity.extend({
 				// damage of 1
 				response.b.loseHealth(1);
 			}
+			}else if (response.b.type==='PlayerEntity') {
+				var xdif = this.pos.x - response.b.pos.x;
+
+				this.attacking = true;
+				// this.lastAttacking = this.now;
+				
+				
+				if(xdif>0){
+					// keeps moving the creep to the right to maintain it's position
+					this.pos.x = this.pos.x + 1;
+					this.body.vel.x = 0;
+				}
+				// checks that it has been at least 1 second since this creep hit something
+				if((this.now-this.lastHit >= 100) && xdif>0) {
+					// updates the lasthit timer
+					this.lastHit = this.now;
+					// makes the player call its loseHealth function and passes at a
+					// damage of 1
+					response.b.loseHealth(1);
+				}
+			}
 		}
-	}
 });
 
 // the timer for the creep
