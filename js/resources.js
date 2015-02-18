556f55df4323208c85abb85d1dbccdc67c41c796
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2015-02-18 11:16:07 -0800

    26. adding title screen

--- js/resources.js
+++ js/resources.js
@@ -10,6 +10,7 @@ game.resources = [
 	{name: "player", type:"image", src: "data/img/orcSpear.png"},
 	{name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
 	{name: "creep1", type:"image", src: "data/img/brainmonster.png"},
+	{name: "title-screen", type:"image", src: "data/img/title.png"},
 	// intermediate /*{name: "playercreep", type:"image", src: "data/img/gloop.png"},*/
 
 	/* Atlases 
