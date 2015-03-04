e16697b96f8180ae92039b6a5aa3f7e79ea92619
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2015-03-04 11:27:14 -0800

    40 continue

--- js/screens/title.js
+++ js/screens/title.js
@@ -35,6 +35,29 @@ game.TitleScreen = me.ScreenObject.extend({
 			}
 		})));
 
+		me.game.world.addChild(new (me.Renderable.extend({
+			// setting the font, width, height, and font size
+			init: function(){
+				this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+				this.font = new me.Font("Arial", 46, "white");
+				// where the pointer should be to start a new game
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+			},
+
+			// putting in the text
+			draw: function(renderer){
+				this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+			},
+
+			update: function(dt) {
+				return true;
+			}, 
+
+			newGame: function() {
+				me.input.registerPointerEvent('pointerdown', this);
+				me.state.change(me.state.PLAY);
+			}
+		})));
 	},
 	
 	
