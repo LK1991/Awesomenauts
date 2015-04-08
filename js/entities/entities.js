ea26d05f68adccee393a5450a299c4b829a8f69b
Lianna Katrdzhyan <lkatrdzhyan@gmail.com>
2015-04-07 18:11:35 -0700

    intermediate 1 (59)

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -36,6 +36,7 @@ game.PlayerEntity = me.Entity.extend({
 	setPlayerTimers: function() {
 		this.now = new Date().getTime();
 		this.lastHit = this.now; //	the last hit makes the tower lose health and fire up
+		this.lastSpear = this.now;
 		this.lastAttack = new Date().getTime(); // Haven't used this
 	},
 
@@ -67,6 +68,7 @@ game.PlayerEntity = me.Entity.extend({
 		// linking functions
 		this.dead = this.checkIfDead();
 		this.checkKeyPressesAndMove();
+		this.checkAbilityKeys();
 		this.setAnimation();
 		me.collision.check(this, true, this.collideHandler.bind(this), true);
 		this.body.update(delta);
@@ -126,6 +128,24 @@ game.PlayerEntity = me.Entity.extend({
 		this.body.jumping = true;
 		this.body.vel.y -= this.body.accel.y * me.timer.tick;
 		me.audio.play("jumping_teon");
+	},
+
+	checkAbilityKeys: function() {
+		if(me.input.isKeyPressed("skill1")) {
+			//this.speedBurst();
+		}else if(me.input.isKeyPressed("skill2")){
+			//this.eatCreep();
+		}else if(me.input.isKeyPressed("skill3")){
+			this.throwSpear();
+		}
+	},
+
+	throwSpear: function() {
+		if(this.lastSpear >= game.data.spearTimer && game.data.ability3 >= 0) {
+			this.lastSpear = this.now;
+			var spear = me.pool.pull("spear", this.pos.x, this.pos.y, {}, );
+			me.game.world.addChild(spear, 10);
+		}
 	},
 
 	// refactoring animations 
--- js/resources.js
+++ js/resources.js
@@ -15,6 +15,7 @@ game.resources = [
 	{name: "exp-screen", type:"image", src: "data/img/loadpic.png"},
 	{name: "load-screen", type:"image", src: "data/img/loadpic.png"},
 	{name: "new-screen", type:"image", src: "data/img/newpic.png"},
+	{name: "spear", type:"image", src: "data/img/spear.png"},
 
 	// intermediate /*{name: "playercreep", type:"image", src: "data/img/gloop.png"},*/
 
